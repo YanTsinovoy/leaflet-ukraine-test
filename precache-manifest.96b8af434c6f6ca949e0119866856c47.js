0de48cafb33495671d7ad838f8e5249f7eb8d5c9
YanTsinovoy <yand199627@gmail.com>
2019-03-17 22:39:39 +0200

    Updates

---
+++ precache-manifest.96b8af434c6f6ca949e0119866856c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leaflet-ukraine-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7467a43ecdd80820eb30",
+    "url": "/leaflet-ukraine-test/static/js/runtime~main.70d77827.js"
+  },
+  {
+    "revision": "8b0946cfe5c8e9a7febf",
+    "url": "/leaflet-ukraine-test/static/js/main.bcfc4ace.chunk.js"
+  },
+  {
+    "revision": "2eaba17d3fda56ada548",
+    "url": "/leaflet-ukraine-test/static/js/2.4cb9d307.chunk.js"
+  },
+  {
+    "revision": "8b0946cfe5c8e9a7febf",
+    "url": "/leaflet-ukraine-test/static/css/main.e59023b8.chunk.css"
+  },
+  {
+    "revision": "2eaba17d3fda56ada548",
+    "url": "/leaflet-ukraine-test/static/css/2.58fcfdef.chunk.css"
+  },
+  {
+    "revision": "206452f2449d879fb384bd19c59749c8",
+    "url": "/leaflet-ukraine-test/index.html"
+  }
+];
